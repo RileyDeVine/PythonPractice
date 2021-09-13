5cd2926c2f2637ff49f3b70365f624921f255f5e
rileydevine <824055@mystma.org>
2021-09-13 09:40:44 -0500

    Added movies list

---
+++ main.py
@@ -0,0 +1,18 @@
+#Print Greeting
+print("Hello World!")
+
+#Print 1st movie
+print("Pacific Rim")
+
+#Print 2nd Movie
+print("Godzilla King Of the Monsters")
+
+#Print 3rd Movie
+print("Godzilla Vs Kong")
+
+#Print 4th Movie
+print("Bumblebee")
+
+#Print 5th Movie
+print("The Snyder Cut")
+
