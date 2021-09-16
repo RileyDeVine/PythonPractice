0b889d6f44079341e565be69898cc625c07070de
rileydevine <824055@mystma.org>
2021-09-16 09:19:02 -0500

    practice with math functions

--- main.py
+++ main.py
@@ -1,11 +1,34 @@
-
+#define times function
 def timesTwo(input):
-
     #multiply input
     print(input * 2)
 
+#define add function
+def addFive(input):
+    #add input
+    (input + 5)
+
+#define divide function
+def divideTwo(input):
+    #divide input
+    (input / 2)
+
+def addTwonumbers (x,y):
+    print(x + y)
+
+addTwonumbers(20, 30)
 #run multiplication function
-timesTwo(4)
+timesTwo(30)
+
+#run add function
+addFive(5)
+
+#run division function
+divideTwo(20)
+
+
+
+
 
 
 
