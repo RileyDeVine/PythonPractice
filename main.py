d6d210ffcb2e18c419328e4632cf70c117df0306
rileydevine <824055@mystma.org>
2021-09-17 09:13:58 -0500

    madlib is now functional

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 #define madlib
-def madLib(placeone, itemone, itempowers, nameone, placetwo, transport, actionone, tradition, adjectiveone):
+def madLib(nounone, nountwo, verbone, nounthree, nounfour, nounfive, adjectiveone, verbtwo, nounsix, adjectivetwo):
     #define setting
-    print("Once upon a time in a place known as " + placeone + ".")
+    print("Once upon a time in a place known as " + nounone + ".")
 
     #define conflict
-    print("Two nations were at war, they had been fighting for centuries over an immensly powerful object known as " + item + ".")
+    print("Two nations were at war, they had been fighting for centuries over an immensly powerful object known as " + nountwo + ".")
 
     #define item powers
-    print("this item had the power to " + itempowers + ".")
+    print("this item had the power to " + verbone + ".")
 
     #define hero arrival
-    print("One day during the war, a new folk came along, they were known as " + nameone + ". They had come from a mystical place up north called " + placetwo + ".")
+    print("One day during the war, a new folk came along, they were known as " + nounthree + ". They had come from a mystical place up north called " + nounfour + ".")
 
     #define steed name
-    print("They rode into the battlefield on a " + transport + ", which were known for being used by valiant heros.")
+    print("They rode into the battlefield on a " + nounfive + ", which were known for being used by" + adjectiveone + " heros.")
 
     #define action
-    print("They dashed through the fighting and grabbed the " + itemone + " and used it to "  + itempowers + ". This caused the two nations to " + actionone)
+    print("They dashed through the fighting and grabbed the " + nountwo + " and used it to "  + verbone + ". This caused the two nations to " + verbone)
 
     #define resolve
-    print("Once the two nations had " + actionone + "they had begun a new tradition known as " + tradition + ". This new tradition had proven very " + adjectiveone + " for the people of the nations.")
+    print("Once the two nations had " + verbtwo + "they had begun a new tradition known as " + nounsix + ". This new tradition had proven very " + adjectivetwo + " for the people of the nations.")
+
+#print welcome message
+print("Welcome to Python MadLibs! Input parameters into the function to create a MadLib.")
+
+madLib("Dumpster", "guy", "swim", "Dude", "Trashheap", "train", "destructive", "lunch", "eating", "underwhelming")
+
