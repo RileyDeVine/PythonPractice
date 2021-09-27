e6d17b86464db662b8a25c7c1f23a1a12a576c21
rileydevine <824055@mystma.org>
2021-09-27 09:07:26 -0500

    tested if statements

--- main.py
+++ main.py
@@ -1,28 +1,12 @@
-#define madlib
-def madLib(nounone, nountwo, verbone, nounthree, nounfour, nounfive, adjectiveone, verbtwo, nounsix, adjectivetwo):
-    #define setting
-    print("Once upon a time in a place known as " + nounone + ".")
+def greaterThan10(x):
+    if x > 10:
+        return "x is greater than 10."
 
-    #define conflict
-    print("Two nations were at war, they had been fighting for centuries over an immensly powerful object known as " + nountwo + ".")
+    elif x==10:
+        return "x equals 10"
+        
+    else:
+        return "x is not greater than 10."
 
-    #define item powers
-    print("this item had the power to " + verbone + ".")
-
-    #define hero arrival
-    print("One day during the war, a new folk came along, they were known as " + nounthree + ". They had come from a mystical place up north called " + nounfour + ".")
-
-    #define steed name
-    print("They rode into the battlefield on a " + nounfive + ", which were known for being used by" + adjectiveone + " heros.")
-
-    #define action
-    print("They dashed through the fighting and grabbed the " + nountwo + " and used it to "  + verbone + ". This caused the two nations to " + verbone)
-
-    #define resolve
-    print("Once the two nations had " + verbtwo + " they had begun a new tradition known as " + nounsix + ". This new tradition had proven very " + adjectivetwo + " for the people of the nations.")
-
-#print welcome message
-print("Welcome to Python MadLibs! Input parameters into the function to create a MadLib.")
-
-madLib("Dumpster", "guy", "swim", "Dude", "Trashheap", "train", "destructive", "lunch", "eating", "underwhelming")
+print(greaterThan10(10))
 
