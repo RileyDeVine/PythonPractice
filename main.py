38641eaaedcc831911cd2e9b0de9087841ef3957
rileydevine <824055@mystma.org>
2021-09-30 09:40:39 -0500

    counting to 10, 10 times

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
+#define function
 def function():
 
+    #define count
     count = 1
-
-    while count < 69419:
-        print("dog")
-        count = count + 1
-
+    countTwo = 1
+    #while count is less than 101
+    while countTwo <= 10:
+        while count <= 10:
+            print(count)
+            count = count + 1
+        countTwo = countTwo + 1
+        count = 1
 
 function()
 
