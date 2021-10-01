469b1af433cd1e8a823d44ce454fc4c71f5ec3ff
rileydevine <824055@mystma.org>
2021-10-01 09:32:45 -0500

    returns last letter of word

--- main.py
+++ main.py
@@ -1,17 +1,9 @@
-#define function
-def function():
+def lastLetter(w):
+    return w[len(w)-1]
 
-    #define count
-    count = 1
-    countTwo = 1
-    #while count is less than 101
-    while countTwo <= 10:
-        while count <= 10:
-            print(count)
-            count = count + 1
-        countTwo = countTwo + 1
-        count = 1
+    
 
-function()
+print(lastLetter("dog"))
+    
 
 
